292f93c0d0770f74d986957e31b5d004ead13fbe
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-01-15 16:53:55 +0100

    refactor hello_triangle

--- src/getting_started/hello_window/main.zig
+++ src/getting_started/hello_window/main.zig
@@ -2,34 +2,40 @@ const std = @import("std");
 const glfw = @import("glfw");
 const gl = @import("gl");
 
-fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
-    _ = p;
-    return glfw.getProcAddress(proc);
-}
+const vertexShaderSource =
+    \\ #version 410 core
+    \\ layout (location = 0) in vec3 aPos;
+    \\ void main()
+    \\ {
+    \\   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\ }
+;
 
-fn framebuffer_size_callback(window: glfw.Window, width: u32, height: u32) void {
-    _ = window;
-    gl.viewport(0, 0, @intCast(c_int, width), @intCast(c_int, height));
-}
+const fragmentShaderSource =
+    \\ #version 410 core
+    \\ out vec4 FragColor;
+    \\ void main() {
+    \\  FragColor = vec4(1.0, 0.5, 0.2, 1.0);   
+    \\ }
+;
 
-fn processInput(window: glfw.Window) void {
-    if (glfw.Window.getKey(window, glfw.Key.escape) == glfw.Action.press) {
-        _ = glfw.Window.setShouldClose(window, true);
-    }
-}
+const WindowSize = struct {
+    pub const width: u32 = 800;
+    pub const height: u32 = 600;
+};
 
 pub fn main() !void {
+
+    // glfw: initialize and configure
+    // ------------------------------
     if (!glfw.init(.{})) {
         std.log.err("GLFW initialization failed", .{});
         return;
     }
     defer glfw.terminate();
 
-    const WindowSize = struct {
-        pub const width: u32 = 800;
-        pub const height: u32 = 600;
-    };
-
+    // glfw window creation
+    // --------------------
     const window = glfw.Window.create(WindowSize.width, WindowSize.height, "mach-glfw + zig-opengl", null, null, .{
         .opengl_profile = .opengl_core_profile,
         .context_version_major = 4,
@@ -41,35 +47,17 @@ pub fn main() !void {
     defer window.destroy();
 
     glfw.makeContextCurrent(window);
-
-    const proc: glfw.GLProc = undefined;
-    try gl.load(proc, glGetProcAddress);
-
     glfw.Window.setFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Triangle in normalized device coordinates
-    const vertices = [9]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
-
-    // The integer handle to our future vertex buffer object (VBO)
-    var VAO: c_uint = undefined;
-    gl.genVertexArrays(1, &VAO);
-    gl.bindVertexArray(VAO);
-
-    // The integer handle to our future vertex buffer object (VBO)
-    var VBO: c_uint = undefined;
-
-    // Create an opengl buffer and store the handle (=1)
-    gl.genBuffers(1, &VBO);
-
-    // Bind the buffer as an ARRAY_BUFFER target
-    gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
-
-    // Fill our buffer with the vertex data
-    gl.bufferData(gl.ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, gl.STATIC_DRAW);
+    // Load all OpenGL function pointers
+    // ---------------------------------------
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
 
     // Create vertex shader
     var vertexShader: c_uint = undefined;
     vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    defer gl.deleteShader(vertexShader);
 
     // Attach the shader source to the vertex shader object and compile it
     gl.shaderSource(vertexShader, 1, @ptrCast([*c]const [*c]const u8, &vertexShaderSource), 0);
@@ -89,6 +77,7 @@ pub fn main() !void {
     // Fragment shader
     var fragmentShader: c_uint = undefined;
     fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    defer gl.deleteShader(fragmentShader);
 
     gl.shaderSource(fragmentShader, 1, @ptrCast([*c]const [*c]const u8, &fragmentShaderSource), 0);
     gl.compileShader(fragmentShader);
@@ -103,6 +92,7 @@ pub fn main() !void {
     // create a program object
     var shaderProgram: c_uint = undefined;
     shaderProgram = gl.createProgram();
+    defer gl.deleteProgram(shaderProgram);
 
     // attach compiled shader objects to the program object and link
     gl.attachShader(shaderProgram, vertexShader);
@@ -115,42 +105,64 @@ pub fn main() !void {
         gl.getProgramInfoLog(shaderProgram, 512, 0, &infoLog);
         std.log.err("{s}", .{infoLog});
     }
-    // Activate shaderProgram
-    gl.useProgram(shaderProgram);
 
-    // delete our shader objects
-    gl.deleteShader(vertexShader);
-    gl.deleteShader(fragmentShader);
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    const vertices = [9]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
+    var VBO: c_uint = undefined;
+    var VAO: c_uint = undefined;
+
+    gl.genVertexArrays(1, &VAO);
+    defer gl.deleteVertexArrays(1, &VAO);
+
+    gl.genBuffers(1, &VBO);
+    defer gl.deleteBuffers(1, &VBO);
+
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    gl.bindVertexArray(VAO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+    // Fill our buffer with the vertex data
+    gl.bufferData(gl.ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, gl.STATIC_DRAW);
 
     // Specify and link our vertext attribute description
     gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), null);
     gl.enableVertexAttribArray(0);
 
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    gl.bindBuffer(gl.ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    gl.bindVertexArray(0);
+
     while (!window.shouldClose()) {
         processInput(window);
 
-        window.swapBuffers();
-        glfw.pollEvents();
-
         gl.clearColor(0.2, 0.3, 0.3, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
+        
+        // Activate shaderProgram
+        gl.useProgram(shaderProgram);
+        gl.bindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+        window.swapBuffers();
+        glfw.pollEvents();
     }
 }
 
-const vertexShaderSource =
-    \\ #version 330 core
-    \\ layout (location = 0) in vec3 aPos;
-    \\ void main()
-    \\ {
-    \\   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    \\ }
-;
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
 
-const fragmentShaderSource =
-    \\ #version 410 core
-    \\ out vec4 FragColor;
-    \\ void main() {
-    \\  FragColor = vec4(1.0, 0.5, 0.2, 1.0);   
-    \\ }
-;
+fn framebuffer_size_callback(window: glfw.Window, width: u32, height: u32) void {
+    _ = window;
+    gl.viewport(0, 0, @intCast(c_int, width), @intCast(c_int, height));
+}
+
+fn processInput(window: glfw.Window) void {
+    if (glfw.Window.getKey(window, glfw.Key.escape) == glfw.Action.press) {
+        _ = glfw.Window.setShouldClose(window, true);
+    }
+}
