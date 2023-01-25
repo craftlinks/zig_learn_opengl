4e6bd40f2f682b8b4332dd2ea28f3b317d33371e
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-01-25 14:55:14 +0100

    we have textures

--- src/getting_started/textures/main.zig
+++ src/getting_started/textures/main.zig
@@ -46,60 +46,6 @@ pub fn main() !void {
     defer arena_allocator_state.deinit();
     const arena = arena_allocator_state.allocator();
 
-    // zstbi: loading an image.
-    zstbi.init(allocator);
-    defer zstbi.deinit();
-
-    const image1_path = common.pathToContent(arena, "content\\container.jpg") catch unreachable;
-    var image1 = try zstbi.Image.init(&image1_path, 0);
-    defer image1.deinit();
-    std.debug.print("Image 1 info:\n\n  img width: {any}\n  img height: {any}\n  nchannels: {any}\n", .{image1.width, image1.height, image1.num_components});
-
-    zstbi.setFlipVerticallyOnLoad(true);
-    const image2_path = common.pathToContent(arena, "content\\awesomeface.png") catch unreachable;
-    var image2 = try zstbi.Image.init(&image2_path, 0);
-    defer image2.deinit();
-    std.debug.print("Image 1 info:\n\n  img width: {any}\n  img height: {any}\n  nchannels: {any}\n", .{image2.width, image2.height, image2.num_components});
-
-    // Create and bind texture1 resource
-    var texture1: c_uint = undefined;
-
-    gl.genTextures(1, &texture1);
-    gl.activeTexture(gl.TEXTURE0); // activate the texture unit first before binding texture
-    gl.bindTexture(gl.TEXTURE_2D, texture1);
-
-    // set the texture1 wrapping parameters
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-    // set texture1 filtering parameters
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-    // Generate the texture1
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(c_int,image1.width), @intCast(c_int,image1.height), 0, gl.RGB, gl.UNSIGNED_BYTE, @ptrCast([*c] const u8, image1.data));
-    gl.generateMipmap(gl.TEXTURE_2D);
-
-    // Texture2
-    var texture2: c_uint = undefined;
-
-    gl.genTextures(1, &texture2);
-    gl.activeTexture(gl.TEXTURE1); // activate the texture unit first before binding texture
-    gl.bindTexture(gl.TEXTURE_2D, texture2);
-
-    // set the texture1 wrapping parameters
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-    // set texture1 filtering parameters
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-    // Generate the texture1
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(c_int,image2.width), @intCast(c_int,image2.height), 0, gl.RGBA, gl.UNSIGNED_BYTE, @ptrCast([*c] const u8, image2.data));
-    gl.generateMipmap(gl.TEXTURE_2D);
-
-
-
-
     // create shader program
     var shader_program: Shader = Shader.create(arena, "content\\shader.vs", "content\\shader.fs");
 
@@ -154,7 +100,61 @@ pub fn main() !void {
     // texture coords
     const tex_offset: [*c]c_uint = (6 * @sizeOf(f32));
     gl.vertexAttribPointer(2, 2, gl.FLOAT, gl.FALSE, 8 * @sizeOf(f32), tex_offset);
-    gl.enableVertexAttribArray(1);
+    gl.enableVertexAttribArray(2);
+
+
+    // zstbi: loading an image.
+    zstbi.init(allocator);
+    defer zstbi.deinit();
+
+    const image1_path = common.pathToContent(arena, "content\\container.jpg") catch unreachable;
+    var image1 = try zstbi.Image.init(&image1_path, 0);
+    defer image1.deinit();
+    std.debug.print("Image 1 info:\n\n  img width: {any}\n  img height: {any}\n  nchannels: {any}\n", .{image1.width, image1.height, image1.num_components});
+
+    zstbi.setFlipVerticallyOnLoad(true);
+    const image2_path = common.pathToContent(arena, "content\\awesomeface.png") catch unreachable;
+    var image2 = try zstbi.Image.init(&image2_path, 0);
+    defer image2.deinit();
+    std.debug.print("Image 2 info:\n\n  img width: {any}\n  img height: {any}\n  nchannels: {any}\n", .{image2.width, image2.height, image2.num_components});
+
+    // Create and bind texture1 resource
+    var texture1: c_uint = undefined;
+
+    gl.genTextures(1, &texture1);
+    gl.activeTexture(gl.TEXTURE0); // activate the texture unit first before binding texture
+    gl.bindTexture(gl.TEXTURE_2D, texture1);
+
+    // set the texture1 wrapping parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    // set texture1 filtering parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    // Generate the texture1
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(c_int,image1.width), @intCast(c_int,image1.height), 0, gl.RGB, gl.UNSIGNED_BYTE, @ptrCast([*c] const u8, image1.data));
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    // Texture2
+    var texture2: c_uint = undefined;
+
+    gl.genTextures(1, &texture2);
+    gl.activeTexture(gl.TEXTURE1); // activate the texture unit first before binding texture
+    gl.bindTexture(gl.TEXTURE_2D, texture2);
+
+    // set the texture1 wrapping parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    // set texture1 filtering parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    // Generate the texture1
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(c_int,image2.width), @intCast(c_int,image2.height), 0, gl.RGBA, gl.UNSIGNED_BYTE, @ptrCast([*c] const u8, image2.data));
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+
 
     shader_program.use();
     shader_program.setInt("texture1", 0);
