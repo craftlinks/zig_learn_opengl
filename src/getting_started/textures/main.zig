9da917704e9560ac66b582ead48bdbeb11649aba
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-01-25 14:19:48 +0100

    textures continued

--- src/getting_started/textures/main.zig
+++ src/getting_started/textures/main.zig
@@ -52,7 +52,7 @@ pub fn main() !void {
     const image_path = common.pathToContent(arena, "content\\container.jpg") catch unreachable;
     var image = try zstbi.Image.init(&image_path, 0);
     defer image.deinit();
-    std.debug.print("img width: {any}\nimg height: {any}\n", .{image.width, image.height});
+    std.debug.print("Texture info:\n\n  img width: {any}\n  img height: {any}\n  nchannels: {any}\n", .{image.width, image.height, image.num_components});
 
     // Create and bind texture resource
     var texture: c_uint = undefined;
@@ -60,22 +60,84 @@ pub fn main() !void {
     gl.genTextures(1, &texture);
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
+    // set the texture wrapping parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    // set texture filtering parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
     // Generate the texture
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(c_int,image.width), @intCast(c_int,image.height), 0, gl.RGB, gl.UNSIGNED_BYTE, @ptrCast([*c] const u8, image.data));
     gl.generateMipmap(gl.TEXTURE_2D);
 
-
     // create shader program
-    // var shader_program: Shader = Shader.create(arena_allocator, "shaders\\shader_ex3.vs", "shaders\\shader_ex3.fs");
+    var shader_program: Shader = Shader.create(arena, "content\\shader.vs", "content\\shader.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
+
+    const vertices = [_]f32{
+        // positions      // colors        // texture coords
+         0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
+         0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
+        -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom left
+        -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top left 
+    };
+
+    const indices = [_]c_uint{
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triangle
+    };
+
+    var VBO: c_uint = undefined;
+    var VAO: c_uint = undefined;
+    var EBO: c_uint = undefined;
+
+    gl.genVertexArrays(1, &VAO);
+    defer gl.deleteVertexArrays(1, &VAO);
+
+    gl.genBuffers(1, &VBO);
+    defer gl.deleteBuffers(1, &VBO);
+
+    gl.genBuffers(1, &EBO);
+    defer gl.deleteBuffers(1, &EBO);
+
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    gl.bindVertexArray(VAO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+    // Fill our buffer with the vertex data
+    gl.bufferData(gl.ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, 6 * @sizeOf(c_uint), &indices, gl.STATIC_DRAW);
+
+    // vertex
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 8 * @sizeOf(f32), null);
+    gl.enableVertexAttribArray(0);
+
+    // colors
+    const col_offset: [*c]c_uint = (3 * @sizeOf(f32));
+    gl.vertexAttribPointer(1, 3, gl.FLOAT, gl.FALSE, 8 * @sizeOf(f32), col_offset);
+    gl.enableVertexAttribArray(1);
+
+    // texture coords
+    const tex_offset: [*c]c_uint = (6 * @sizeOf(f32));
+    gl.vertexAttribPointer(2, 2, gl.FLOAT, gl.FALSE, 8 * @sizeOf(f32), tex_offset);
+    gl.enableVertexAttribArray(1);
+
+    shader_program.use();
 
     while (!window.shouldClose()) {
         processInput(window);
 
         gl.clearColor(0.0, 0.0, 0.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.bindVertexArray(VAO);
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, null);
 
         window.swapBuffers();
         glfw.pollEvents();
